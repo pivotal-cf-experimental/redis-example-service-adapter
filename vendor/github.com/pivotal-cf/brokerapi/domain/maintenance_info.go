87d2acf3123a535121a648a4a28e9118061af7f9
Derik Evangelista <devangelista@pivotal.io>
2019-10-04 12:11:12 +0100

    Update on-demand-services-sdk and vendor
    Changed yaml.v2 to consume releases rather than branch
    
    Co-authored-by: George Blue <gblue@pivotal.io>

--- vendor/github.com/pivotal-cf/brokerapi/domain/maintenance_info.go
+++ vendor/github.com/pivotal-cf/brokerapi/domain/maintenance_info.go
@@ -3,11 +3,14 @@ package domain
 import "reflect"
 
 type MaintenanceInfo struct {
-	Public  map[string]string `json:"public,omitempty"`
-	Private string            `json:"private,omitempty"`
-	Version string            `json:"version,omitempty"`
+	Public      map[string]string `json:"public,omitempty"`
+	Private     string            `json:"private,omitempty"`
+	Version     string            `json:"version,omitempty"`
+	Description string            `json:"description,omitempty"`
 }
 
 func (m *MaintenanceInfo) Equals(input MaintenanceInfo) bool {
-	return reflect.DeepEqual(*m, input)
+	return m.Version == input.Version &&
+		m.Private == input.Private &&
+		reflect.DeepEqual(m.Public, input.Public)
 }
